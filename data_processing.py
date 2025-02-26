ca28323d3ec0dd9b19f15733ccec3f6256a62028
sahil-tapkir <sahiltapkir07@gmail.com>
2025-02-26 11:43:23 +0530

    Corrected code that cause a error in training that was indexing of :1 and also train the model

--- data_processing.py
+++ data_processing.py
@@ -15,7 +15,7 @@ labels=[];
 DATA_DIR="./data";
 
 for dir_ in os.listdir(DATA_DIR):
-  for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+  for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
     data_aux=[];
     img=cv2.imread(os.path.join(DATA_DIR,dir_,img_path))
     img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from collections import Counter
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -7,6 +8,9 @@ from sklearn.metrics import accuracy_score
 data_dict=pickle.load(open('./data.pickle','rb'))
 data=np.asarray(data_dict['data'])
 labels=np.asarray(data_dict['labels'])
+
+class_counts = Counter(labels)
+print(class_counts)
 
 x_train,x_test,y_train,y_test=train_test_split(data,labels,test_size=0.2,shuffle=True,stratify=labels)
 
